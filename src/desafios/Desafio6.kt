180888a0ee299cfb2193a681798e135a71c06e54
Marcelo Alves <marceloalves1295@gmail.com>
2021-03-02 14:36:02 -0300

    Innit commit

--- src/desafios/Desafio6.kt
+++ src/desafios/Desafio6.kt
@@ -1,5 +1,6 @@
 package desafios
 
+import kotlin.math.sqrt
 /*
    |------------------------------------------------------------------------------------------------------------------|
    | Mariazinha sabe que um Número Primo é aquele que pode ser dividido somente por 1 (um) e por ele mesmo.           |
@@ -21,34 +22,34 @@ package desafios
    |------------------------------------------------------------------------------------------------------------------|
  */
 fun main() {
-
-    val lista = mutableListOf<String>()
-    val qtd = readLine()!!.toInt()
-
-    for (i in 1..qtd) {
-
-        val numeroPrimo = readLine()!!.toDouble()
-        val conversao:Int = numeroPrimo.toInt()
-        if (ehPrimo(conversao)){
-
-            lista.add("Prime")
-        }
-        else {
-            lista.add("Not Prime")
-        }
+    val n = readLine()!!.toInt()
 
 
-    }
-    for (i in lista) {
-        println(i)
+    for (i in 0 until n) {
+        val x = readLine()!!.toDouble()
+        val prime = isPrime(x)
+        print(prime)
+
+
     }
 }
-private fun ehPrimo(numero: Int): Boolean {
-    for (j in 2 until numero) {
-        if (numero % j == 0){
-            return false
-        }
+
+fun print(prime: Boolean) {
+    if (prime) {
+        print("Prime\n")
+    } else print("Not Prime\n")
+}
+
+fun isPrime(num: Double): Boolean {
+
+    if (num < 2) return false
+    if (num % 2 == 0.0) return num == 2.0
+    val root = sqrt(num).toInt()
+    var i = 3
+    while (i <= root) {
+        if (num % i == 0.0) return false
+        i += 2
     }
     return true
-}
 
+}
